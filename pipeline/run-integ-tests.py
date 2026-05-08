9b65e3277f0c7c18815f22e880cf40e652aca77a
loachri <loachri@amazon.com>
2026-05-07 18:46:08 -0700

    fix: Skip Redshift submitter on macOS (SIP strips DYLD_LIBRARY_PATH)
    Signed-off-by: loachri <loachri@amazon.com>

--- pipeline/run-integ-tests.py
+++ pipeline/run-integ-tests.py
@@ -44,8 +44,9 @@ def main():
 
     args = ["mayapy", "-m", "pytest", "--no-cov", "test/integ", "-vvv", "--numprocesses=1"]
     # macOS: skip adaptor tests (no SMF rendering support, native extension ABI issues)
+    # Also skip Redshift submitter (plugin can't load due to SIP stripping DYLD_LIBRARY_PATH)
     if system == "Darwin":
-        args += ["--ignore=test/integ/test_maya_adaptors.py"]
+        args += ["--ignore=test/integ/test_maya_adaptors.py", "-k", "not Redshift"]
 
     sys.exit(subprocess.run(args).returncode)
 
